8f73261ea1a3b69c7552fa5d9e7b1f718cd8da67
Fabio <clonefabster7@yahoo.com>
2023-08-28 17:58:32 -0700

    Return reversed string

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 // Given two values, return the first one if it is falsy, otherwise return the second one
 
-function filterOutFalsy (value1, value2) {
-return (!value1) ? value1 : value2;
+function filterOutFalsy(value1, value2) {
+  return !value1 ? value1 : value2;
 }
 
-console.log(filterOutFalsy(0, 5))
-console.log(filterOutFalsy(false, 100))
-console.log(filterOutFalsy(true, 'Dog'))
+console.log(filterOutFalsy(0, 5));
+console.log(filterOutFalsy(false, 100));
+console.log(filterOutFalsy(true, "Dog"));
 
-
-//Return length of any given array 
+//Return length of any given array
 
 /*function arrLength (arr) {
     return arr.length;
@@ -22,71 +21,81 @@ console.log(arrLength([]))
 
 //Get the last element in an array
 
-function lastElem (arr) {
-    return arr[arr.length - 1]
+function lastElem(arr) {
+  return arr[arr.length - 1];
 }
 
-console.log(lastElem([1, 2, 3, 4]))
-console.log(lastElem([1, 2, 3, 'dog']))
-console.log(lastElem([null, 5, false]))
+console.log(lastElem([1, 2, 3, 4]));
+console.log(lastElem([1, 2, 3, "dog"]));
+console.log(lastElem([null, 5, false]));
 
 //Find the sum of an array
 
-function arrSum (arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; ++i) {
-
-        sum = sum + arr[i];
-    }
-    return sum;
+function arrSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; ++i) {
+    sum = sum + arr[i];
+  }
+  return sum;
 }
 
 console.log(arrSum([2, 2, 2]));
 console.log(arrSum([300, 100, 200, 200]));
 
-
 // Given a number, add up all the numbers from one to the number that is given.
 
-function progressiveSum (numb) {
-let sum = 0;
-for (let i = 1; i <= numb; ++i) {
+function progressiveSum(numb) {
+  let sum = 0;
+  for (let i = 1; i <= numb; ++i) {
     sum = sum + i;
-}
-return sum;
+  }
+  return sum;
 }
 
 console.log(progressiveSum(3));
 console.log(progressiveSum(6));
 console.log(progressiveSum(10));
 
-
 //Given a number in seconds, return this number in mm:ss format
 
-function calcTime (seconds) {
-let timerMinutes = Math.floor(seconds / 60);    
-let timerSeconds = seconds % 60;
+function calcTime(seconds) {
+  let timerMinutes = Math.floor(seconds / 60);
+  let timerSeconds = seconds % 60;
 
-if (timerMinutes.toString().length === 1) {
-    timerMinutes = '0' + timerMinutes
+  if (timerMinutes.toString().length === 1) {
+    timerMinutes = "0" + timerMinutes;
+  }
+
+  return timerMinutes + ":" + timerSeconds;
 }
 
-return timerMinutes + ':' + timerSeconds;
-}
-
-console.log(calcTime(600))
-console.log(calcTime(300))
+console.log(calcTime(600));
+console.log(calcTime(300));
 
 //Find the largest number
 
-function getMax (arr) {
-    let max = arr[0];
-    for (i = 0; i < arr.length; ++i) {
-     if (arr[i] > max) {
-        max = arr[i];
-     }
+function getMax(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; ++i) {
+    if (arr[i] > max) {
+      let max = arr[i];
     }
-return max;
+  }
+  return max;
 }
 
-console.log(getMax([1, 3, 5]))
-console.log(getMax([-100, -200, -500]))
+console.log(getMax([1, 3, 5]));
+console.log(getMax([-100, -200, -500]));
+
+
+//Given a string, return the reversed string
+
+function reverseString (str) {
+    let reversedString = '';
+    let (i = 0; i < str.length; ++i) {
+ reversedString = str[i] + reverseString;
+    }
+    return reverseString;
+}
+
+console.log(reverseString('abc'));
